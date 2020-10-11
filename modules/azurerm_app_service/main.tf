2445b23a17efc2e0caadefcdd2b56a41e74c9624
Adit Modi <adit.modi24@gmail.com>
2020-10-11 11:33:34 +0530

    Update main.tf

--- modules/azurerm_app_service/main.tf
+++ modules/azurerm_app_service/main.tf
@@ -5,6 +5,7 @@ resource "azurerm_app_service" "app_service" {
   resource_group_name = var.resource_group_name
   app_service_plan_id = var.app_service_plan_id
 
+  
   https_only = true
 
   site_config {
